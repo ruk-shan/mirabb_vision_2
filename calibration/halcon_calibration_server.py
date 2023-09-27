2deaa54110b099ee12e00c437b0ae10e6272a255
ruk-shan <ruk.shan@outlook.com>
2023-09-27 14:25:54 +0800

    updated calibration code.

--- calibration/halcon_calibration_server.py
+++ calibration/halcon_calibration_server.py
@@ -3,6 +3,7 @@ from time import sleep
 import config 
 import json
 from getkey import getkey, key
+from colorama import Fore
   
 # create and configure socket on local host  
 serverSocket = socket.socket()   
@@ -16,40 +17,27 @@ con, addr = serverSocket.accept()
 connected = True  
 print( "connected to client" )
 
-
 # send_message = ''
 recv_message = 'YY'
 CaliPos = -1
 
 #send robot pos to halcon
 def get_robot_pos(line):
-    print ("---------------")
-    print (line)
-    count = 1
-    while True:
-        try:
-            f = open("calibration/robot_pos.txt", "r")
-            file  = f.read()
-            file  = f.close()
-            json_object = json.loads(file)
-            val_to_return = json_object[str(line)]  
-            return val_to_return
-        except:
-            count += 1
-            if count > 10:
-                print ("unable to read 'robot_pos.txt'")
-                break
+    val_to_return = config.robot_poses[line] 
+    print ("robot_pose" + val_to_return)
+    return val_to_return
+
 
 def move_robot_to_pos(pos):
-    print("Press enter after the robot moved to pos_" + str(pos))
+    print(Fore.GREEN +"     Press enter after, the robot moved to pos_" + str(pos))
     while True:
     # Get the pressed key
         var = getkey()
     # Print the pressed key
-        print(var)
         if var == key.ENTER:
         # If the ENTER key is pressed, break the While loop.
-            print("You pressed enter, Hope the robot is at pos_" + str(pos))
+            print(Fore.BLUE +"     You've pressed enter, Hope the robot is at pos_" + str(pos))
+            print (Fore.WHITE)
             break  
     return True
 
@@ -60,25 +48,35 @@ while True:
         #resposing to recv_messages
         if (recv_message == 'Ball'):
             print ('received Ball')
+            sleep(.5)
             con.send( bytes("ok","UTF-8" ) ) 
             print ('sent msg : ok')
+            sleep(.5)
 
         elif (recv_message == 'get_pos'):
-            send_message = str(get_robot_pos(CaliPos))
-            print ("received 'get_pos  '+send_message")
-
+            print("getting robot pose_"+ str(CaliPos))
+            send_message = get_robot_pos(CaliPos)
+            sleep(.5)
             print(f'sending message....:{send_message}')
             con.send( bytes(send_message, "UTF-8" ) ) 
+            sleep(.5)
 
         elif (recv_message == 'Home'):
-            send_message = "ok"
             print ("received 'home'")
+            sleep(.5)
             con.send( bytes("ok, moved to home.", "UTF-8" ) ) 
+            sleep(.5)
+        
+        elif (recv_message == 'DONE'):
+            print ("I'm done, Bye")
+            break
         
         elif (CaliPos >-1):
             print ("received CaliPos"+ str(CaliPos))
             move_robot_to_pos(CaliPos)
+            sleep(.5)
             con.send( bytes("ok, moved to CaliPos_"+ str(CaliPos), "UTF-8" ) ) 
+            sleep(.5)
         
         else:
             send_message = "ok"
@@ -86,19 +84,15 @@ while True:
 
         print ('listening ...')
         recv_message = con.recv(1024).decode('utf-8') 
-        print( recv_message )  
         split_msg = recv_message.split("_")
-        if (len(split_msg)) >1 and isinstance(split_msg[1], int):
-            
+        if (len(split_msg)) >1 and split_msg[0]== "CaliPos":
             CaliPos = int(split_msg[1])
-            print (CaliPos)
-        else:
-            CaliPos = -1
+            # print (f"CaliPos: {CaliPos}")
+
 
     except  socket.error: 
         con, addr = serverSocket.accept()  
         print( "connection lost... reconnecting" )  
-    # wait 1 second  
 
         while not connected:  
             # attempt to reconnect, otherwise sleep for 2 seconds  
@@ -108,5 +102,5 @@ while True:
                 print( "re-connection successful" )  
             except socket.error:  
                 sleep( 2 )  
-  
+
 con.close();  
